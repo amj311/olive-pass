c8e8f79296639c3eafec95bcdb267d6be93b30af
amj311 <amjudd315@gmail.com>
2021-06-20 17:41:00 -0600

    added email conf constants

--- model/Constants.js
+++ model/Constants.js
@@ -2,6 +2,8 @@ module.exports = {
   Colors: {
     Primary: "rgb(100, 212, 28)"
   },
+  EmailCodeLength: 6,
+  EmailCodeExpiration: 60000 * 60 * 1, // 1 hour
   SessionDuration: 60000 * 60 * 24, // 1 day
   PasswordRegex: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20})/,
   EmailRegex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
